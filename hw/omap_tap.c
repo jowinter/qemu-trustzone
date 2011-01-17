ebb28c691e77a40dd37af89e28a28722be1eae65
Riku Voipio <riku.voipio@nokia.com>
2011-01-17 16:50:37 +0000

    hw/omap_tap.c: prepare for omap3
    omap_tap updates for omap3
    
    Signed-off-by: Juha Riihimäki <juha.riihimaki@nokia.com>

--- hw/omap_tap.c
+++ hw/omap_tap.c
@@ -17,7 +17,6 @@
  * You should have received a copy of the GNU General Public License along
  * with this program; if not, see <http://www.gnu.org/licenses/>.
  */
-
 #include "hw.h"
 #include "omap.h"
 
@@ -36,13 +35,15 @@ static uint32_t omap_tap_read(void *opaque, target_phys_addr_t addr)
         case omap2430:
             return 0x5b68a02f;	/* ES 2.2 */
         case omap3430:
-            return 0x1b7ae02f;	/* ES 2 */
+            return 0x4b7ae02f;  /* ES 3.1 */
+        case omap3630:
+            return 0x1b89102f;  /* ES 1.1 */
         default:
-            hw_error("%s: Bad mpu model\n", __FUNCTION__);
+            hw_error("%s: Bad mpu model", __FUNCTION__);
         }
 
-    case 0x208:	/* PRODUCTION_ID_reg for OMAP2 */
-    case 0x210:	/* PRODUCTION_ID_reg for OMAP3 */
+    case 0x208: /* PRODUCTION_ID_reg bits 0-31 */
+    case 0x210: /* PRODUCTION_ID reg bits 64-95 */
         switch (s->mpu_model) {
         case omap2420:
             return 0x000254f0;	/* POP ESHS2.1.1 in N91/93/95, ES2 in N800 */
@@ -53,12 +54,13 @@ static uint32_t omap_tap_read(void *opaque, target_phys_addr_t addr)
         case omap2430:
             return 0x000000f0;
         case omap3430:
-            return 0x000000f0;
+        case omap3630:
+            return 0x000f00f0;
         default:
-            hw_error("%s: Bad mpu model\n", __FUNCTION__);
+            hw_error("%s: Bad mpu model", __FUNCTION__);
         }
-
-    case 0x20c:
+    case 0x20c: /* PRODUCTION_ID bits 32-63 */
+    case 0x214: /* PRODUCTION_ID bits 96-127 */
         switch (s->mpu_model) {
         case omap2420:
         case omap2422:
@@ -67,9 +69,11 @@ static uint32_t omap_tap_read(void *opaque, target_phys_addr_t addr)
         case omap2430:
             return 0xcafeb68a;	/* ES 2.2 */
         case omap3430:
-            return 0xcafeb7ae;	/* ES 2 */
+            return 0xcafeb7ae;	/* ES 2.x/3.0 */
+        case omap3630:
+            return 0xcafeb891;  /* ES 1.0/1.1 */
         default:
-            hw_error("%s: Bad mpu model\n", __FUNCTION__);
+            hw_error("%s: Bad mpu model", __FUNCTION__);
         }
 
     case 0x218:	/* DIE_ID_reg */
@@ -87,7 +91,7 @@ static uint32_t omap_tap_read(void *opaque, target_phys_addr_t addr)
 }
 
 static void omap_tap_write(void *opaque, target_phys_addr_t addr,
-                uint32_t value)
+                           uint32_t value)
 {
     OMAP_BAD_REG(addr);
 }
@@ -105,8 +109,9 @@ static CPUWriteMemoryFunc * const omap_tap_writefn[] = {
 };
 
 void omap_tap_init(struct omap_target_agent_s *ta,
-                struct omap_mpu_state_s *mpu)
+                   struct omap_mpu_state_s *mpu)
 {
-    omap_l4_attach(ta, 0, l4_register_io_memory(
-                            omap_tap_readfn, omap_tap_writefn, mpu));
+    omap_l4_attach(ta, 0, l4_register_io_memory(omap_tap_readfn,
+                                                omap_tap_writefn,
+                                                mpu));
 }
