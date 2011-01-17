57ff4f5c0cebe17299a2efd6f4321fb86978742f
Matt Waddel <matt.waddel@ubuntu.com>
2011-01-17 16:50:36 +0000

    During byte wide writes the i2c write routine gets into an infinite loop. The STP bit in the status register is not set so the i2c_end_transfer() routine is never being called, even when the data buffer is empty. Moving the i2c_end_transfer() call outside the STP check seems to fix this problem.

--- hw/omap_i2c.c
+++ hw/omap_i2c.c
@@ -151,8 +151,8 @@ static void omap_i2c_fifo_run(struct omap_i2c_s *s)
         }
         if (!s->count_cur) {
             TRACE("no more data to transmit/receive");
+            i2c_end_transfer(s->bus);
             if ((s->control >> 1) & 1) {   /* STP */
-                i2c_end_transfer(s->bus);
                 s->control &= ~0x0602;     /* MST | TRX | STP */
                 s->count_cur = s->count;
             } 
