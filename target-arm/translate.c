9adcdfd48e092999c135aed48b396595715a17b5
Peter Maydell <peter.maydell@linaro.org>
2011-01-18 15:43:56 +0000

    Catch UNDEF cases for VDUP (scalar)
    Catch the UNDEF cases for VDUP(scalar):
     imm4 == x000
     Q == 1 && Vd<0> == 1

--- target-arm/translate.c
+++ target-arm/translate.c
@@ -5717,6 +5717,9 @@ static int disas_neon_data_insn(CPUState * env, DisasContext *s, uint32_t insn)
                 dead_tmp(tmp);
             } else if ((insn & 0x380) == 0) {
                 /* VDUP */
+                if ((insn & (7 << 16)) == 0 || (q && (rd & 1))) {
+                    return 1;
+                }
                 if (insn & (1 << 19)) {
                     tmp = neon_load_reg(rm, 1);
                 } else {
