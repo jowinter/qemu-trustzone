2b986aa9223124aa314816788f691c01189a451c
Peter Maydell <peter.maydell@linaro.org>
2011-01-18 16:13:21 +0000

    Use 'q' in identifying Neon '3 regs different lengths' forms
    Bit 6 of the instruction has already been pulled out into the
    'q' variable, so use that rather than reextracting the bit.

--- target-arm/translate.c
+++ target-arm/translate.c
@@ -4990,7 +4990,7 @@ static int disas_neon_data_insn(CPUState * env, DisasContext *s, uint32_t insn)
     } else { /* (insn & 0x00800010 == 0x00800000) */
         if (size != 3) {
             op = (insn >> 8) & 0xf;
-            if ((insn & (1 << 6)) == 0) {
+            if (!q) {
                 /* Three registers of different lengths.  */
 
                 if (op == 15
