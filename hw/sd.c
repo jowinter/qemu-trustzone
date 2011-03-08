6f67da7be76b0dcded8e5fb2798a45841f068c25
Peter Maydell <peter.maydell@linaro.org>
2011-03-08 12:16:40 +0000

    hw/sd.c: Don't crash in sd_set_cb() if no block device present
    Fix a segfault when a board calls sd_set_cb() when there is no block
    device present for this SD card.
    
    Signed-off-by: Peter Maydell <peter.maydell@linaro.org>

--- hw/sd.c
+++ hw/sd.c
@@ -504,8 +504,13 @@ void sd_set_cb(SDState *sd, qemu_irq readonly, qemu_irq insert)
 {
     sd->readonly_cb = readonly;
     sd->inserted_cb = insert;
-    qemu_set_irq(readonly, bdrv_is_read_only(sd->bdrv));
-    qemu_set_irq(insert, bdrv_is_inserted(sd->bdrv));
+    if (sd->bdrv) {
+        qemu_set_irq(readonly, bdrv_is_read_only(sd->bdrv));
+        qemu_set_irq(insert, bdrv_is_inserted(sd->bdrv));
+    } else {
+        qemu_set_irq(readonly, 0);
+        qemu_set_irq(insert, 0);
+    }
 }
 
 static void sd_erase(SDState *sd)
