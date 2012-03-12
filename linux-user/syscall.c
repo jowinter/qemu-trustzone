9611b00dc18745b2b2e2780e13b8862c761029ec
Peter Maydell <peter.maydell@linaro.org>
2012-03-12 15:03:56 +0000

    linux-user: Fix compilation failure for ppc64abi32-linux-user targets
    Fix compilation failure for ppc64abi32-linux-user targets where we were
    using the wrong format string specifier to print an abi_ulong.

--- linux-user/syscall.c
+++ linux-user/syscall.c
@@ -4669,8 +4669,9 @@ static int open_self_maps(void *cpu_env, int fd)
             continue;
         }
         if (h2g_valid(min) && h2g_valid(max)) {
-            dprintf(fd, TARGET_FMT_lx "-" TARGET_FMT_lx " %c%c%c%c %08" PRIx64
-                    " %02x:%02x %d%s%s\n", h2g(min), h2g(max), flag_r, flag_w,
+            dprintf(fd, TARGET_ABI_FMT_lx "-" TARGET_ABI_FMT_lx
+                    " %c%c%c%c %08" PRIx64 " %02x:%02x %d%s%s\n",
+                    h2g(min), h2g(max), flag_r, flag_w,
                     flag_x, flag_p, offset, dev_maj, dev_min, inode,
                     path[0] ? "          " : "", path);
         }
